68005e8a6ff88ac18218be862d55031524c8b3ef
unknown <charles.weld@mydit.ie>
2016-03-26 15:05:16 +0000

    lasjkdf

---
+++ GradeApp/src/gradeapp/GradeApp.java
@@ -0,0 +1,23 @@
+package gradeapp;
+
+import java.util.ArrayList;
+
+public class GradeApp {
+	
+	// Class variables
+	private String passwords[];
+	private ArrayList<String> grades;
+	private boolean loggedIn;
+	
+	public GradeApp(){
+		
+	}
+	public boolean login(int num, String pass){
+		return true;
+	}
+	
+	public String getGrades(){
+		return "grade1";
+	}
+
+}
